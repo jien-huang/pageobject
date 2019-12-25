d8dba17f6bdfdd9081da7af8964d24878b6f3799
pangyazhi <pangyazhi@gmail.com>
2019-12-25 19:11:58 +0000

    add content to about tab

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,8 +17,32 @@ export class AppComponent implements OnInit {
     { name: 'attributes', value: 'id,name,type,value,text,href,title,hidden,tagName' },
     { name: 'header', value: 'import Page from \'./page-model\''}
   ];
+  testExample =`
+  import TestcafeExample_Page from './TestcafeExample_Page';
+
+fixture \`My fixture\`
+    .page \`https://devexpress.github.io/testcafe/example/\`;
+
+      
+test('TestCafe Example', async _ => {
+
+    var ids = [
+        { id: 'tried-test-cafe', value: 'on' },
+        { id: 'background-parallel-testing', value: 'on' },
+        { id: 'developer-name', value: 'firstname lastname' },
+        { id: 'remote-testing', value: 'on' }
+    ]
+    var _page = new TestcafeExample_Page()
+    await _page.setData(ids);
+    var objs = {'CI':'on', 'windows': 'on'}
+    await _page.setData(objs);
+    
+    await _page.click(_page.CLICKABLE.POPULATE);
+    await _page.click(_page.CLICKABLE.SUBMIT_BUTTON);
+});
+  `
   about = `
-  import { Selector, t } from 'testcafe';
+import { Selector, t } from 'testcafe';
 
 export default class Page {
   constructor() {
